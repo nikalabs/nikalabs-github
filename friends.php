55b85513fbc51b8560096542d01fd586fd041aea
nikalabs <nazaninmohammadpour@Nazanins-iMac.local>
2014-12-24 17:09:53 -0800

    update

---
+++ friends.php
@@ -0,0 +1,31 @@
+
+   <!-- Start of Main Body -->
+   <div class="main">
+    <div class="container">
+
+      <div class="page-header">
+        <h1>Friends<small> <form class="navbar-form navbar-left f-form" role="search">
+        <div class="friend-search-wrapper">
+        <div class="form-group frinds-search">
+          <input type="text" class="form-control search-txt" placeholder="Search for Friends" id="friend-search">
+                <button type="button" class="btn close"><img src="images/close-icon.png"></button>
+        </div>
+      </div>
+      </form></small></h1>
+      </div>
+      <div class="row"><!-- Start of the First Row -->
+        <div class="result-found"><p>8 Search Results Found</p></div>
+        <div class="col-md-1 friend first"><div class="wrapper"><div class="round"><img src="images/placeholder-1.png"><button type="button" class="btn addedfriend"><span class="glyphicon glyphicon-ok"></span></buton></div></div><div class="triends-name"><span class="first-name">John</span> <span class="last-name">Smith</span></div></div>
+        <div class="col-md-1 friend"><div class="wrapper"><div class="round"><img style="background-color:#e8e8e8;" src="images/placeholder-2.png"><button type="button" class="btn addfriend"><span class="glyphicon glyphicon-plus"></span></buton></div></div><div class="triends-name"><span class="first-name">Jane</span> <span class="last-name">Smith</span></div></div>
+        <div class="col-md-1 friend"></div>
+        <div class="col-md-1 friend"></div>
+        <div class="col-md-1 friend"></div>
+        <div class="col-md-1 friend"></div>
+        <div class="col-md-1 friend"></div>
+        <div class="col-md-1 friend"></div>
+
+
+      </div><!-- End of the First Row -->
+      </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
