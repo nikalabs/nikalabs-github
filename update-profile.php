5cf087cda5a3dfaf343a231c5e2bec65ef960265
nikalabs <nazaninmohammadpour@Nazanins-iMac.local>
2014-12-26 09:23:26 -0800

    update

---
+++ update-profile.php
@@ -0,0 +1,50 @@
+
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+ <div class="row"><!-- Start of the Row -->
+ 
+  	      
+<div class="col-md-6"><!-- Start of the Column -->
+      <div class="page-header">
+        <h1>Update Profile</h1>
+      </div>
+      <form class="update-form" role="sign-up" action="mailto:youremail@email.com" method="post">
+        <div class="form-group">
+          <div class="first-name">First Name<br/>
+          <input type="text" class="form-control firts" placeholder="John" name="first" id="first"></div>
+          <div class="last-name">Last Name<br/>
+          <input type="text" class="form-control last" placeholder="Smith" name="last" id="last"></div>
+           <div class="username">Username<br/>
+          <input type="text" class="form-control user" placeholder="JohnSmith4382@target.co" name="user" id="user"></div>
+          <div class="password">Password<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></div>
+          <div class="again">Again<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></div>
+          <span class="info">*Password must be strong and contain 1 letter, 1 number, and 1 special character.</span>
+
+          <div class="p-image">Profile Image<br>
+         <div class="image-upload">
+         
+         <label for="file-input">
+            <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
+        <img src="images/placeholder-1.png"/>
+         </label>
+         <input id="file-input" type="file"/>
+  </div></div>
+        </div>
+        <div class="upgrade-div"><a href="#" target="_blank"><button type="submit" class="upgrade">+Upgrade to Business Account</a></button></div>
+        <button type="submit" class="btn btn-default cancel"> Cancel</buton>
+        <button type="submit" class="btn btn-default update">Save Changes</button>
+      </form>
+     
+     
+      
+    </div><!-- End of the Column -->
+          
+      </div><!-- End of the Row -->
+
+     
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
