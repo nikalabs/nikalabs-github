6bc5674078e78cfe728f1f732af8819b4360268c
nikalabs <nazaninmohammadpour@Nazanins-iMac.local>
2014-12-26 14:20:08 -0800

    update

---
+++ signup-page-2.php
@@ -0,0 +1,56 @@
+
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+ <div class="row"><!-- Start of the First Row -->
+  <div class="col-md-6 first"><!-- Start of the First Column -->
+   	<div class="page-header">
+        <h1 style="color:transparent;">Sign Up</h1>
+      </div>  
+<p><strong>For Consumers:</strong><br/>
+Find ALL local New, Fresh, and On-promotion Specials from ALL local businesses daily anytime and anywhere!<br/>
+It is free and always will be!</p>
+
+<p><strong>For Local Businesses: </strong></br>
+Engage ALL local consumers with efficiency, effectiveness, and more revenue and profit generation daily.</p>
+ 
+<p><strong>For Schools or Charities:</strong></br>
+Sharing of FrontApron's 10% annual revenue by simply signing up charity members.</p>
+
+<p>Both Business and School/Charity simply Sign Up and then Upgrade.</p>
+     
+  	  
+  	</div><!-- End of the First Column -->
+  	      
+<div class="col-md-6 second"><!-- Start of the Second Column -->
+      <div class="page-header">
+        <h1>Sign Up</h1>
+      </div>
+      <form class="signup-form" role="sign-up" action="mailto:youremail@email.com" method="post">
+        <div class="form-group">
+          <p class="first-name">First Name<br/>
+          <input type="text" class="form-control firts" placeholder="John" name="first" id="first"></p>
+          <p class="last-name">Last Name<br/>
+          <input type="text" class="form-control last" placeholder="Smith" name="last" id="last"></p>
+           <p>E-mail or Mobile Number<br/>
+          <input type="text" class="form-control user" placeholder="JohnSmith4382@target.co" name="user"></p>
+          <p class="password">Password<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></p>
+          <p class="again">Again<br/>
+          <input type="password" class="form-control pass" id="pass" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" name="password" size="8" ></p>
+          <span class="info">*Password must be strong and contain 1 letter, 1 number, and 1 special character.</span>
+        </div>
+        <button type="submit" class="btn btn-default sign-up">+ Create Account</button>
+      </form>
+     
+     
+      
+    </div><!-- End of the Second Column -->
+          
+      </div><!-- End of the First Row -->
+
+     
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
+  
