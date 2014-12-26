b98ceff41bb02c9fcdf9e6012aa994a7d7f95527
nikalabs <nazaninmohammadpour@Nazanins-iMac.local>
2014-12-26 15:44:14 -0800

    update

---
+++ payment-page.php
@@ -0,0 +1,109 @@
+
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+       
+ <div class="row"><!-- Start of the First Row -->
+   <div class="col-md-6 pay"><!-- Start of the First Column -->
+<div class="page-header">
+        <h1>Payment Information</h1>
+      </div>
+  
+<div class="payment-info">
+  <div id="creditcard">
+    <table class="table" id="credit-table">
+      <tr>
+    <td><div class="card-name">Name on Card:</div></td>
+    <td><span class="name-on-card">John Smith</span></td>
+    
+  </tr>
+  <tr>
+    <td><div class="ccnumber">Credit Card Number:</div></td>
+    <td><span class="credit-card-number">&#9679;&#9679;&#9679;&#9679;-&#9679;&#9679;&#9679;&#9679;-&#9679;&#9679;&#9679;&#9679;-4312</span></td>
+    
+  </tr>
+   <tr>
+    <td><div class="exp-date">Expiration Date: </div></td>
+    <td><span class="expiration-date">09 / 2016</span></td>
+    
+  </tr>
+  <tr>
+    <td><div class="address">Expiration Date: </div></td>
+    <td><span class="ad">1243 1<sup>st</sup> Street San Jose CA 95008</span></td>
+    
+  </tr>
+  </table>
+          
+ 
+</div>
+
+
+</div>
+          
+       
+          
+       
+        </div> <!-- End of the First Column -->
+              <div class="col-md-6 pay"><!-- Start of the Second Column -->
+             
+    </div><!-- End of the Second Column -->
+
+        </div><!-- End of the First Row -->
+
+
+
+           <div class="row"><!-- Start of the Second Row -->
+  <div class="col-md-6 "><!-- Start of the First Column -->
+   	  <div class="page-header">
+        <h1>Payment History</h1>
+      </div>
+       <table class="table" id="market-table">
+         <tr  class="table-header">
+    <th>Date</th>
+    <th>Special Name</th>
+    <th>Start Date</th>
+    <th>End Date</th>
+    <th>Amount</th>
+
+  </tr>
+  <tr>
+    <td>Dec 24 2014</td>
+    <td>Starbucks</td>
+    <td>Dec 21 2014</td>
+    <td>Dec 24 2014</td>
+    <th>$30</th>
+  </tr>
+  <tr>
+    <td>Dec 24 2014</td>
+    <td>Starbucks</td>
+    <td>Dec 21 2014</td>
+    <td>Dec 24 2014</td>
+    <th>$30</th>
+  </tr>
+   <tr>
+    <td>Dec 24 2014</td>
+    <td>Starbucks</td>
+    <td>Dec 21 2014</td>
+    <td>Dec 24 2014</td>
+    <th>$30</th>
+  </tr>
+ 
+       </table>
+
+       
+     
+  	  
+  	</div><!-- End of the First Column -->
+  	      
+    <div class="col-md-6 second"><!-- Start of the Second Column -->
+             
+    </div><!-- End of the Second Column -->
+    
+          
+      </div><!-- End of the First Row -->
+
+     
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
+ 
