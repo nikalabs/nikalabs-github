c938f319ed3041f63b2f846247b190e585a64bd0
nikalabs <nazaninmohammadpour@Nazanins-iMac.local>
2014-12-26 09:43:52 -0800

    update

---
+++ activation-page.php
@@ -0,0 +1,29 @@
+
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+ <div class="row"><!-- Start of the First Row -->
+  <div class="col-md-6 first"><!-- Start of the First Column -->
+   	  <div class="page-header">
+        <h1>Activation</h1>
+      </div>
+      <form class="activation-form " role="activate" action="mailto:youremail@email.com" method="post">
+        <div class="form-group">
+          <div><br/>
+          <input type="text" class="form-control activation" placeholder="" name=""></div>
+        
+        </div>
+        <button type="submit" class="btn btn-default activation">Activate</button>
+      </form>
+     
+  	  
+  	</div><!-- End of the First Column -->
+  	      
+
+      </div><!-- End of the First Row -->
+
+     
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
+   
