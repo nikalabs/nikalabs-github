d04cbaf30bc0dcbaf26856862247c9c0a42c1b70
nikalabs <nazaninmohammadpour@Nazanins-iMac.local>
2015-01-02 18:02:06 -0800

    update

---
+++ index2.php
@@ -0,0 +1,170 @@
+<!doctype html>
+ <html>
+ <head>
+      	 <meta charset="utf-8">
+      	 <meta name="viewport" content="width=device-weidth, initial-scale=1.0">
+      	 <title>Front Apron</title>
+      	 <link href="css/bootstrap.min.css" rel="stylesheet">
+      	 <link href="css/custom.css" rel="stylesheet">
+      	 <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+ </head>
+ <body>
+   <header>
+ 	 <div class="container">
+ 		<a class="navbar-brand" href="index.html"><img src="images/logo.png"></a>
+    <form class="navbar-form search" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="Coffee">
+                  <input type="text" class="form-control" placeholder="San Jose">
+                </div>
+                <button type="submit" class="btn btn-default search"><img src="images/search-icon.png"></button>
+        </form>
+      <form class="head-signin-form home" role="sign-in" action="mailto:youremail@email.com" method="post">
+        <div class="form-group">
+           <button type="submit" class="btn btn-default log">Log In</button>
+        <input type="button" class="btn log" id="log" value="Sign Up" name="sign_up">
+      </div>
+      </form>
+ 		
+        
+       
+  
+ 		
+ 	</div>
+   </header>
+ <!-- Start of Navigation Bar -->
+   
+<!-- Start of Main Body -->
+   <div class="main">
+   	<div class="container">
+
+   	  <div class="page-header" style="margin-top:10px; border-bottom:0px;">
+       
+      </div>
+      <div class="row"><!-- Start of the First Row -->
+  	   <div class="col-md-3 first-column"><!-- Start of the First Column -->
+  	   	<div class="thumbnail">
+          <div class="image-container">
+            <div class="after">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#"><span>SAVE</span> <img src="images/save-icon.png"></a></li>
+            <li><a href="#"><span>SHARE</span> <img src="images/share-icon.png"></a></li>
+            <li><a href="#"><span>TRACK</span> <img src="images/track-icon.png"></a></li>
+          </ul>
+          </div>
+             <img class="no-margin" src="images/thumbnail-1.png" alt="home-thumb">
+          
+
+            </div>
+          
+           <div class="caption">
+             <h3>Freshly brewed coffee<img class="company-logo" src="images/company-logo-starbucks.png" alt="star-bucks"></h3>
+                <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+ 
+  	    	  	<h5><img src="images/location-pin.png"> San Jose <small>$10</small></h5>
+  	    		  	    	  	     
+  	       </div>
+
+  	    </div>
+
+  	</div><!-- End of the First Column -->
+  	        <div class="col-md-3 first-column"><!-- Start of the Second Column -->
+  	        	<div class="thumbnail">
+          <div class="image-container">
+              <div class="after">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#"><span>SAVE</span> <img src="images/save-icon.png"></a></li>
+            <li><a href="#"><span>SHARE</span> <img src="images/share-icon.png"></a></li>
+            <li><a href="#"><span>TRACK</span> <img src="images/track-icon.png"></a></li>
+          </ul>
+            </div>
+            <img class="no-margin" src="images/thumbnail-1.png" alt="home-thumb">
+            </div>
+           <div class="caption">
+             <h3>Freshly brewed coffee<img class="company-logo" src="images/company-logo-starbucks.png" alt="star-bucks"></h3>
+                <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+
+               <h5><img src="images/location-pin.png"> San Jose <small>$10</small></h5>
+  	       </div>
+  	      </div>
+  	  </div><!-- End of the Second Column -->
+  	    <div class="col-md-3"><!-- Start of the Third Column -->
+  	    	<div class="thumbnail">
+           <img class="no-margin" src="images/thumbnail-1.png" alt="home-thumb">
+           <div class="caption">
+             <h3>Freshly brewed coffee<img class="company-logo" src="images/company-logo-starbucks.png" alt="star-bucks"></h3>
+               <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+
+               <h5><img src="images/location-pin.png"> San Jose <small>$10</small></h5>
+  	       </div>
+  	      </div>
+  	    </div><!-- End of the Third Column -->
+        <div class="col-md-3"><!-- Start of the Fourth Column -->
+          <div class="thumbnail">
+           <img class="no-margin" src="images/thumbnail-1.png" alt="home-thumb">
+           <div class="caption">
+             <h3>Freshly brewed coffee<img class="company-logo" src="images/company-logo-starbucks.png" alt="star-bucks"></h3>
+               <p>Our coffee masters have distilled their years 
+of tasting knowledge down to three simple 
+questions to help you find a coffee you’re ...</p>
+
+               <h5><img src="images/location-pin.png"> San Jose <small>$10</small></h5>
+           </div>
+          </div>
+        </div><!-- End of the Fourth Column -->
+
+      </div><!-- End of the First Row -->
+
+      
+
+    </div><!-- End of the Container -->
+   
+   </div> <!-- End of the Main -->
+   <!-- Start of the Footer -->
+   <footer class="footer">
+   	<div class="container">
+
+   	 <div class="row top">
+   	 	<div class="col-md-8">
+   	 		<ul class="footer-menu">
+   	 			<li><a href="#">About FrontApron</a></li>
+          <li><a href="#">Business</a></li>
+   	 			<li><a href="#">Privacy Policy</a></li>
+   	 			<li><a href="#">Terms & Conditions</a></li>
+   	 			<li><a href="#">Help</a></li>
+   	 		</ul>
+
+      <p class="copy-right">© 2014, FrontApron.  All Rights Reserved.</p>
+     
+   	 	</div><!-- End of the Footer First Column -->
+   	 	<div class="col-md-4">
+   	 	
+   	 	 <div class="footer-icons">
+   	 	  <a class="google-play"href="#"><img src="images/google-play.png"></a>
+   	 	  <a class="appstore"href="#"><img src="images/appstore.png"></a>
+   	 	 </div>
+   	 	 <div class="footer-socials">
+   	 	 	<a class="fbook"><img src="images/facebook.png"></a>
+   	 	 	<a class="twitter"><img src="images/twitter.png"></a>
+   	 	 	<a class="gplus"><img src="images/googleplus.png"></a>
+   	 	 </div>
+   	 	</div><!-- End of the Footer Third Column -->
+   	 </div><!-- End of the Footer Row Top -->
+   	 
+   	</div><!-- End of the Footer Container -->
+   </footer>
+   <!-- End of the Footer -->
+
+ <script src="js/jquery.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ </body>
+ </html>
